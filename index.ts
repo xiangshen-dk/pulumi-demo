27a76921bc77135344e74f59b4fa07402bcc6881
Xiang Shen <shen.xiang@gmail.com>
2023-04-15 03:15:07 +0000

    Change permission & add index file

--- index.ts
+++ index.ts
@@ -3,8 +3,25 @@ import * as gcp from "@pulumi/gcp";
 
 // Create a GCP resource (Storage Bucket)
 const bucket = new gcp.storage.Bucket("my-bucket", {
-    location: "US"
+    location: "US",
+    website: {
+        mainPageSuffix: "index.html"
+    },
+    uniformBucketLevelAccess: true
+});
+
+const bucketIAMBinding = new gcp.storage.BucketIAMBinding("my-bucket-IAMBinding", {
+    bucket: bucket.name,
+    role: "roles/storage.objectViewer",
+    members: ["allUsers"]
+});
+
+const bucketObject = new gcp.storage.BucketObject("index.html", {
+    name: "index.html",
+    bucket: bucket.name,
+    source: new pulumi.asset.FileAsset("index.html")
 });
 
 // Export the DNS name of the bucket
 export const bucketName = bucket.url;
+export const bucketEndpoint = pulumi.concat("http://storage.googleapis.com/", bucket.name, "/", bucketObject.name);
